f38f81a1a89747e4fd0bd8c2d808e07f3bdb2890
Jonathan Santos <jonathan.g.o.santos@gmail.com>
2026-09-12 14:37:42 -0300

    Algoritmo RR funcionando

--- src/AlgoritmoRR.java
+++ src/AlgoritmoRR.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class AlgoritmoRR implements Algoritmo {
     private int quantum;
@@ -12,6 +12,34 @@ public class AlgoritmoRR implements Algoritmo {
 
     @Override
     public Metricas executar(List<Processo> processos) {
-        return null;
+        Queue<Processo> fila = new LinkedList<>();
+        int ultimoAChegar = processos.stream().max(Comparator.comparing(Processo::getTempoChegada)).get().getTempoChegada();
+        int tempo = 0;
+        int execucoes = 0;
+        while (tempo <= ultimoAChegar || !fila.isEmpty()) {
+            int finalTempo = tempo;
+            fila.addAll(processos.stream().filter(p -> p.getTempoChegada().equals(finalTempo)).toList());
+            Processo primeiro = fila.peek();
+            if (primeiro != null) {
+                primeiro.setTempoPrimeiraExecucao(tempo);
+                primeiro.executar();
+                execucoes++;
+                if (primeiro.concluido()) {
+                    fila.remove(primeiro);
+                    primeiro.setTempoConclusao(tempo);
+                    execucoes = 0;
+                } else if (execucoes == quantum) {
+                    fila.remove(primeiro);
+                    execucoes = 0;
+                    fila.add(primeiro);
+                }
+            }
+            tempo++;
+        }
+
+        double mediaTempoResposta = (double) processos.stream().map(Processo::getTempoResposta).map(Optional::get).reduce(Integer::sum).get() / processos.size();
+        double mediaTempoEspera = (double) processos.stream().map(Processo::getTempoEspera).map(Optional::get).reduce(Integer::sum).get() / processos.size();;
+        double mediaTurnaround = (double) processos.stream().map(Processo::getTurnaround).map(Optional::get).reduce(Integer::sum).get() / processos.size();;
+        return new Metricas(mediaTempoResposta, mediaTempoEspera, mediaTurnaround);
     }
 }
